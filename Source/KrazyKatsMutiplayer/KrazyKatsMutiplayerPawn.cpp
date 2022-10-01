f74c299925aa8aa4f5319d127cf64e6c80b35db3
M.Abdullah Sultan <abdullahsultan448@gmail.com>
2022-10-01 22:21:57 +0500

    UE5 Upgrade

--- Source/KrazyKatsMutiplayer/KrazyKatsMutiplayerPawn.cpp
+++ Source/KrazyKatsMutiplayer/KrazyKatsMutiplayerPawn.cpp
@@ -45,26 +45,26 @@ AKrazyKatsMutiplayerPawn::AKrazyKatsMutiplayerPawn()
 	// Simulation
 	UWheeledVehicleMovementComponent4W* Vehicle4W = CastChecked<UWheeledVehicleMovementComponent4W>(GetVehicleMovement());
 
-	check(Vehicle4W->WheelSetups.Num() == 4);
+	//check(Vehicle4W->WheelSetups.Num() == 4);
 
-	Vehicle4W->WheelSetups[0].WheelClass = UKrazyKatsMutiplayerWheelFront::StaticClass();
-	Vehicle4W->WheelSetups[0].BoneName = FName("Wheel_Front_Left");
-	Vehicle4W->WheelSetups[0].AdditionalOffset = FVector(0.f, -12.f, 0.f);
+	//Vehicle4W->WheelSetups[0].WheelClass = UKrazyKatsMutiplayerWheelFront::StaticClass();
+	//Vehicle4W->WheelSetups[0].BoneName = FName("Wheel_Front_Left");
+	//Vehicle4W->WheelSetups[0].AdditionalOffset = FVector(0.f, -12.f, 0.f);
 
-	Vehicle4W->WheelSetups[1].WheelClass = UKrazyKatsMutiplayerWheelFront::StaticClass();
-	Vehicle4W->WheelSetups[1].BoneName = FName("Wheel_Front_Right");
-	Vehicle4W->WheelSetups[1].AdditionalOffset = FVector(0.f, 12.f, 0.f);
+	//Vehicle4W->WheelSetups[1].WheelClass = UKrazyKatsMutiplayerWheelFront::StaticClass();
+	//Vehicle4W->WheelSetups[1].BoneName = FName("Wheel_Front_Right");
+	//Vehicle4W->WheelSetups[1].AdditionalOffset = FVector(0.f, 12.f, 0.f);
 
-	Vehicle4W->WheelSetups[2].WheelClass = UKrazyKatsMutiplayerWheelRear::StaticClass();
+	/*Vehicle4W->WheelSetups[2].WheelClass = UKrazyKatsMutiplayerWheelRear::StaticClass();
 	Vehicle4W->WheelSetups[2].BoneName = FName("Wheel_Rear_Left");
 	Vehicle4W->WheelSetups[2].AdditionalOffset = FVector(0.f, -12.f, 0.f);
 
 	Vehicle4W->WheelSetups[3].WheelClass = UKrazyKatsMutiplayerWheelRear::StaticClass();
 	Vehicle4W->WheelSetups[3].BoneName = FName("Wheel_Rear_Right");
-	Vehicle4W->WheelSetups[3].AdditionalOffset = FVector(0.f, 12.f, 0.f);
+	Vehicle4W->WheelSetups[3].AdditionalOffset = FVector(0.f, 12.f, 0.f);*/
 
 	// Create a spring arm component
-	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm0"));
+	/*SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm0"));
 	SpringArm->TargetOffset = FVector(0.f, 0.f, 200.f);
 	SpringArm->SetRelativeRotation(FRotator(-15.f, 0.f, 0.f));
 	SpringArm->SetupAttachment(RootComponent);
@@ -72,16 +72,16 @@ AKrazyKatsMutiplayerPawn::AKrazyKatsMutiplayerPawn()
 	SpringArm->bEnableCameraRotationLag = true;
 	SpringArm->CameraRotationLagSpeed = 7.f;
 	SpringArm->bInheritPitch = false;
-	SpringArm->bInheritRoll = false;
+	SpringArm->bInheritRoll = false;*/
 
 	// Create camera component 
-	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera0"));
+	/*Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera0"));
 	Camera->SetupAttachment(SpringArm, USpringArmComponent::SocketName);
 	Camera->bUsePawnControlRotation = false;
-	Camera->FieldOfView = 90.f;
+	Camera->FieldOfView = 90.f;*/
 
 	// Create In-Car camera component 
-	InternalCameraOrigin = FVector(0.0f, -40.0f, 120.0f);
+	/*InternalCameraOrigin = FVector(0.0f, -40.0f, 120.0f);
 
 	InternalCameraBase = CreateDefaultSubobject<USceneComponent>(TEXT("InternalCameraBase"));
 	InternalCameraBase->SetRelativeLocation(InternalCameraOrigin);
@@ -90,7 +90,7 @@ AKrazyKatsMutiplayerPawn::AKrazyKatsMutiplayerPawn()
 	InternalCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("InternalCamera"));
 	InternalCamera->bUsePawnControlRotation = false;
 	InternalCamera->FieldOfView = 90.f;
-	InternalCamera->SetupAttachment(InternalCameraBase);
+	InternalCamera->SetupAttachment(InternalCameraBase);*/
 
 	//Setup TextRenderMaterial
 	static ConstructorHelpers::FObjectFinder<UMaterial> TextMaterial(TEXT("Material'/Engine/EngineMaterials/AntiAliasedTextMaterialTranslucent.AntiAliasedTextMaterialTranslucent'"));
